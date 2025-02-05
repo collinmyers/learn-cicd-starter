405a6fdf9adf5accaffefe38fa83237bcab47834
Collin Myers <collinmyers98@gmail.com>
2025-02-04 20:27:18 -0500

    Update test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -20,13 +20,13 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			name:     "malformed auth header",
 			headers:  map[string][]string{"Authorization": {"Bearer"}},
-			expected: "123",
+			expected: "",
 			err:      ErrMalformedAuthHeader,
 		},
 		{
 			name:     "valid auth header",
 			headers:  map[string][]string{"Authorization": {"ApiKey 123"}},
-			expected: "33333333",
+			expected: "123",
 			err:      nil,
 		},
 	}
