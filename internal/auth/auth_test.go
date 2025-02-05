ce34e934f554279837d7afa8cc274a84c6682c53
Collin Myers <collinmyers98@gmail.com>
2025-02-04 20:12:57 -0500

    added tests and updated action file, temp fail in code

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name     string
+		headers  map[string][]string
+		expected string
+		err      error
+	}{
+		{
+			name:     "no auth header",
+			headers:  map[string][]string{},
+			expected: "",
+			err:      ErrNoAuthHeaderIncluded,
+		},
+		{
+			name:     "malformed auth header",
+			headers:  map[string][]string{"Authorization": {"Bearer"}},
+			expected: "",
+			err:      ErrMalformedAuthHeader,
+		},
+		{
+			name:     "valid auth header",
+			headers:  map[string][]string{"Authorization": {"ApiKey 123"}},
+			expected: "123",
+			err:      nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiKey, err := GetAPIKey(tt.headers)
+			if apiKey != tt.expected {
+				t.Errorf("expected %s, got %s", tt.expected, apiKey)
+			}
+			if err != tt.err {
+				t.Errorf("expected %v, got %v", tt.err, err)
+			}
+		})
+	}
+}
